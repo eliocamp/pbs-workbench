bb48dd61bf39250ad4d3bb70940776a11d4c7dd3
Elio Campitelli <eliocampitelli@gmail.com>
2026-07-21 16:24:51 +1000

    Ads modules to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 # This needs to be run on a compute node
+module purge
+module load patchelf
+module load gcc/15.1.0
+
 python -m nuitka  \
  --onefile   \
  --include-package=job   \
  --include-data-files=src/job/data/idle.sh=job/data/idle.sh   \
  --include-package=urwid   \
  --output-filename=job   \
- --remove-output \ 
- src/job/cli.py
+ --remove-output src/job/cli.py
