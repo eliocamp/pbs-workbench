26ecc4ade7b33a9769c519cda7a45fa864730a3a
Elio Campitelli <eliocampitelli@gmail.com>
2026-08-31 13:52:19 +1000

    Ensures that paths exist

--- src/job/places.py
+++ src/job/places.py
@@ -1,21 +1,33 @@
 import os 
+from functools import wraps
+
+def ensure_path(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        path = func(*args, **kwargs)
+        os.makedirs(path, exist_ok = True)
+        return path
+    return wrapper
 
 def home() -> str:
     return os.environ["HOME"]
-    
+
+@ensure_path
 def workbench_home():
     return f"{home()}/pbs-workbench"
 
+@ensure_path
 def workbench_dir() -> str:
     return f"{workbench_home()}/workbenches/"
     
-
+@ensure_path
 def workbench_file() -> list[str]:
     return f"{workbench_dir()}/00"
 
-
+@ensure_path
 def profile_folder():
     return f"{workbench_home()}/profiles"
 
+@ensure_path
 def logs_folder():
     return f"{workbench_home()}/logs"
