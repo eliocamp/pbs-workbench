02d25fc0ad11b054c2d8265af6dfd45dd44ef9b9
Elio Campitelli <eliocampitelli@gmail.com>
2026-09-24 15:07:41 +1000

    Adds release checks

--- release.sh
+++ release.sh
@@ -6,8 +6,18 @@ version=$(grep -Po  "version = \"\K\d.\d.\d" $proj)
 compiled_version=$(./job --version)
 
 if [ $version != $compiled_version ]; then
-  echo "Compiled version ($compiled_version) does not match project version ($version)"
+  echo "Compiled version ($compiled_version) does not match project version ($version)."
   exit 1
 fi
+
+if ! git diff --quiet || ! git diff --cached --quiet; then
+  echo "There are uncommited changes." 
+  exit 1
+fi
+
+if [ -n "$(git log '@{u}..HEAD')"]; then
+  echo "There are local commits not pushed to remote."
+  exit 1
+fi 
 
 gh release create $version job --generate-notes 
